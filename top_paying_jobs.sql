7f5ab27b34461d21e34d8b585f0e76ba9fb6e890
MD Wasim Akhtar <wasimtichu@gmail.com>
2024-05-18 00:36:16 +0530

    Completed

--- top_paying_jobs.sql
+++ top_paying_jobs.sql
@@ -3,3 +3,14 @@
 -Focus on jobs with specific salary(remove nulls)
 */
 
+SELECT
+job_id, job_title, job_location, job_schedule_type, salary_year_avg, job_posted_date,
+name AS company_name
+FROM
+job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id=company_dim.company_id
+where
+job_title = 'Data Analyst'AND
+job_location = 'Anywhere' AND salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10;
